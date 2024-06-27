bc58088b8af569b4432d1c630b0ee736a905e3c3
codeniaaz <muktarabdiaziz12@gmail.com>
2024-06-27 06:11:06 +0300

    9-try.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,4 +4,4 @@ export default function handleResponseFromAPI(promise) {
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => { console.log('Got a response from the API'); });
-}i
+}
